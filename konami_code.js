ec947badd4975d10665d955a414a5f157aec7c91
Heshie Brody <4hbrody@gmail.com>
2019-08-05 19:48:23 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,24 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.addEventListener('keydown', onKeyDownHandler);
 }
+
+function onKeyDownHandler (event) {
+  const key = parseInt(event.which || event.detail);
+
+  if (key === code[index]) {
+    index++;
+
+    if (index === code.length) {
+      alert("Konami!!!!");
+
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
+
+// init();
